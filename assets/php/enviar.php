8b80f4f1106ddb3b46e5e9dc41f13f5d5c89ebf9
Ary <arydeg@gmail.com>
2020-12-25 18:35:08 -0300

    v1

--- assets/php/enviar.php
+++ assets/php/enviar.php
@@ -1,37 +1,32 @@
-
 <?php
-if(isset($_POST['email'])) {
-
-<bold>// Debes editar las próximas dos líneas de código de acuerdo con tus preferencias</bold>
-$email_to = "arydeg@gmail.com";
-$email_subject = "Contacto desde el sitio web";
-
-<bold>// Aquí se deberían validar los datos ingresados por el usuario</bold>
-if(!isset($_POST['name']) ||
-//!isset($_POST['last_name']) ||
-!isset($_POST['email']) ||
-//!isset($_POST['telephone']) ||
-!isset($_POST['message'])) {
-
-echo "<b>Ocurrió un error y el formulario no ha sido enviado. </b><br />";
-echo "Por favor, vuelva atrás y verifique la información ingresada<br />";
-die();
-}
-
-$email_message = "Detalles del formulario de contacto:\n\n";
-$email_message .= "Nombre: " . $_POST['name'] . "\n";
-//$email_message .= "Apellido: " . $_POST['last_name'] . "\n";
-$email_message .= "E-mail: " . $_POST['email'] . "\n";
-//$email_message .= "Teléfono: " . $_POST['telephone'] . "\n";
-$email_message .= "Mensaje: " . $_POST['message'] . "\n\n";
-
-
-<bold>// Ahora se envía el e-mail usando la función mail() de PHP</bold>
-$headers = 'From: '.$email_from."\r\n".
-'Reply-To: '.$email_from."\r\n" .
-'X-Mailer: PHP/' . phpversion();
-@mail($email_to, $email_subject, $email_message, $headers);
-
-echo "¡El formulario se ha enviado con éxito!";
-}
+    $nombre = $_POST['name'];
+    $email = $_POST['email'];
+    $mensaje = $_POST['message'];
+    $para = 'arydeg@gmail.com';
+    echo "<script language='javascript'>
+    alert('hola');
+    </script>";
+    $header = 'From: ' . $email;
+    $msjCorreo = "Nombre: $nombre\n E-Mail: $email\n Asunto:\n $mensaje";
+        echo "<script language='javascript'>
+    alert('hola2');
+    </script>";  
+    /*if ($_POST['submit']) 
+    {
+                echo "<script language='javascript'>
+    alert('hola3');
+    </script>";
+        if (mail($para, $subject, $msjCorreo, $header)) 
+        {
+            echo "<script language='javascript'>
+            alert('Mensaje enviado, muchas gracias.');
+            </script>";
+        } 
+        else 
+        {
+            echo "<script language='javascript'>
+            alert('fallado');
+            </script>";
+        }
+    } */
 ?>
