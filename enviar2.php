8273fef18d70646edf16a719341c7276ac88f9d1
Ary <arydeg@gmail.com>
2020-12-25 18:50:42 -0300

    trato de enviado de mail v3

--- enviar2.php
+++ enviar2.php
@@ -1,7 +1,7 @@
 <?php
-$nombre = $_POST['name'];
+$nombre = $_POST['nombre'];
 $mail = $_POST['email'];
-$empresa = $_POST['message'];
+$empresa = $_POST['mensaje'];
 
 $header = 'From: ' . $mail . " \r\n";
 $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
@@ -13,7 +13,7 @@ $mensaje .= "Su e-mail es: " . $mail . " \r\n";
 $mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
 $mensaje .= "Enviado el " . date('d/m/Y', time());
 
-$para = 'arydeg@gmail.com';
+$para = 'ignaciobluuweb@gmail.com';
 $asunto = 'Mensaje de mi sitio web';
 
 mail($para, $asunto, utf8_decode($mensaje), $header);
